ddbab2d8c8b9054ab829d6bee10349f9ea179f9c
Sanskar Mishra <sanskar.mishra@gmail.com>
2021-06-14 19:29:56 -0500

    added visual components

--- grid.py
+++ grid.py
@@ -1,6 +1,11 @@
 from pprint import pprint
-class Grid:
-    def __init__(self):
+from ship import Ship
+import pygame
+
+
+class Grid (pygame.sprite.Sprite):
+    def __init__(self, computer_board):
+        super().__init__()
         self.board_size = 10
 
         self.hit_board = [
@@ -11,9 +16,31 @@ class Grid:
             [False for j in range(self.board_size)]
             for i in range(self.board_size)
         ]
+        self.ships = []
 
-        pprint(self.hit_board)
-        pprint(self.ship_board)
+        self.image = pygame.Surface((720-30 , 720-30) if computer_board
+                                    else (20 * self.board_size, 20 * self.board_size))
+        self.rect = pygame.rect.Rect((15, 15, self.image.get_width(), self.image.get_height()) if computer_board
+                                     else (1280/2, 710 - self.image.get_height(),self.image.get_width(), self.image.get_height()))
+        self.image.fill((255,255,255) if computer_board else (0,0,0))
+        # pprint(self.hit_board)
+        # pprint(self.ship_board)
+
+    def add_ship(self, ship):
+        for pos in ship.positions:
+            if 0 <= pos[1] < self.board_size and 0 <= pos[0] < self.board_size and self.ship_board[pos[1]][pos[0]]:
+                print("AHHHHHHHHHHHHHHHHHHHHHHHHHH")
+                return False
+        self.ships.append(ship)
+        for pos in ship.positions:
+            self.ship_board[pos[1]][pos[0]] = True
+        return True
+
 
 if __name__ == '__main__':
     g = Grid()
+    g.add_ship(Ship((4, 6), "up", 3))
+    g.add_ship(Ship((4, 6), "up", 3))
+    print(f"length: {len(g.ships)}" )
+
+    pprint(g.ship_board)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+from grid import Grid
 pygame.init()
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
@@ -7,13 +7,18 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("GTA 6")
 clock = pygame.time.Clock()
 isGameRunning = True
+player_grid = Grid(False)
+computer_grid = Grid(True)
+grid_group = pygame.sprite.Group(player_grid, computer_grid)
 while isGameRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isGameRunning = False
     screen.fill((100,0,255))
     pygame.draw.circle(screen,(0,0,0),(600,600),20)
+    grid_group.draw(screen)
     pygame.display.flip()
+
     clock.tick(60)
 
 pygame.quit()
